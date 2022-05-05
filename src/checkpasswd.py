c594fba20c941be022fb19a5f0a01af705f03982
wudale111 <375255433@qq.com>
2022-05-05 11:55:38 +0800

    Update checkpasswd.py

--- src/checkpasswd.py
+++ src/checkpasswd.py
@@ -85,7 +85,7 @@ def check(s):
     if a + b + c + d < 3:
         return 0
     #遍历长度为3的子字符串，以这个子串进行切割，只有两段则没有相同长度为3的子串。
-#     for i in range(len(s) - 3):
+#     for i in range(len(s) - 2):
 #         if len(s.split(s[i:i + 3])) >= 3:
 #             return 0
 #     return 1
